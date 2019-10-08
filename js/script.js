c1faee55846941791b0e9a8d9052c0e00437586f
de-arevalo <olaveraalleinad@gmail.com>
2019-10-08 01:08:39 +0000

    worked on if else

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 $( "document" ).ready(function() {
-    $("#button").click(function(){
-        $(".output").show();
-    });
+    //function piglatinize()
+        $("#button").click(function(){
+          //  if () {
+            let input = $(".input").val();
+            let words = input.split(" ")
+            console.log(words + "!")
+            console.log(input)
+            let vowels = ["a", "e", "i", "o", "u"]
+            words.forEach(function(word){
+                console.log(word)
+                index = 0
+                let beginning = word.charAt(index);
+                console.log(beginning)
+                console.log(vowels.find("a"))
+                if (beginning == vowels) {
+                    $("input").append("ay");
+                } else {
+                    $("")
+                }
+            })
+            //let end = str.split(input.charAt(2));
+            //$(".output").append(input);
+           // }
+        });
+    //});
 });
